354e010f9865bfe34b28d91b738f53e6e34f1783
tomaszgrazul <tgra@wp.pl>
2023-01-17 20:54:26 +0100

    zrobione

---
+++ main.js
@@ -0,0 +1,8 @@
+//let a = document.getElementById("opis");
+let but = document.querySelector('button');
+but.onclick = function() 
+{
+    let paragraf = document.querySelector('p');
+    paragraf.textContent = 'Modyfikacja paragrafu';
+}
+
